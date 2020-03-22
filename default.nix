5fc8b28f8d73ab79a0b3dca74fa0f238e5ebc9c6
Compl Yue <compl.yue@icloud.com>
2020-03-22 19:38:23 +0800

    upgrade to edh-0.2.0.0

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
 import (
 # to use a version of Edh from github
   builtins.fetchTarball {
-    url = "https://github.com/e-wrks/edh/archive/0.1.0.1.tar.gz";
-    sha256 = "00bcpx6liszkp7arykxmb971i2a3mpkpsgziig450iz884b9vdcw";
+    url = "https://github.com/e-wrks/edh/archive/0.2.0.0.tar.gz";
+    sha256 = "0fsckwasy2gf67rz72acq0af3m31mx2ljj2xdz1dvrlwpryb6q85";
   }
 
   # to use the version of Edh checked out locally
